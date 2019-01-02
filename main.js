ebd59a7119d8fd8cd97e0b44efc38d0fb1d6427f
Joeri <joeri.deplecker@hotmail.com>
2019-01-02 23:02:59 +0100

    Rect rendering

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
+var main;
+var mainrenderscene;
+
 function mainInit() {
+	main = document.getElementById("main");
+	mainrenderscene = document.getElementById("mainrenderscene");
 	renderInit();
-	bindMouse();
+	initMouse();
 }
 
-function bindMouse() {
-	// bind mouse
+function initMouse() {
+	main.onmouseover= function(e) { 
+		player.angle+=(e.layerX - W/2)/W * 0.1;
+		drawScreen();
+		if(e.layerY < H/3) {
+			player.yaw-=0.05;
+			drawScreen();
+		}
+		if(e.layerY > 2*H/3) {
+			player.yaw+=0.05;
+			drawScreen();
+		}
+	}
+}
+
+function keyEvent(e) {
+	switch(e.which) {
+		case(90): player.z +=0.1; break;
+		case(68): player.x +=0.1; break;
+		case(81): player.x -=0.1; break;
+		case(83): player.z -=0.1; break;
+	}
+	console.log(player);
+	drawScreen();
 }
--- render.js
+++ render.js
@@ -1,3 +1,5 @@
+var xmlns = "http://www.w3.org/2000/svg";
+
 var vertices = [];
 var sectors = [];
 var player;
@@ -28,11 +30,11 @@ function intersectBox(x0, y0, x1, y1, x2, y2, x3, y3) {
 // Calculate intersecting point between 2 lines
 function intersect(a1, a2, b1, b2) {
 	var x1 = new Vector2D(a1.vxs(a2), a1.x - a2.x);
-	var x2 = new Vector2D(a3.vxs(a4), a3.x - a4.x);	
+	var x2 = new Vector2D(b1.vxs(b2), b1.x - b2.x);	
 	var y1 = new Vector2D(a1.vxs(a2), a1.y - a2.y);
-	var y2 = new Vector2D(a1.vxs(a2), a3.y - a4.y);
+	var y2 = new Vector2D(a1.vxs(a2), b1.y - b2.y);
 	var n1 = new Vector2D(a1.x - a2.x, a1.y - a2.y);
-	var n2 = new Vector2D(a3.x - a4.x, a3.y - a4.y);
+	var n2 = new Vector2D(b1.x - b2.x, b1.y - b2.y);
 	var x = x1.vxs(x2) / n1.vxs(n2);
 	var y = y1.vxs(y2) / n1.vxs(n2);
 	
@@ -41,6 +43,7 @@ function intersect(a1, a2, b1, b2) {
 
 function renderInit() {
 	loadData();
+	drawScreen();
 }
 
 function loadData() {
@@ -62,12 +65,12 @@ function loadData() {
 		var i = 2;
 		while(i < sector.length/2 + 1){
 			vertexList.push(sector[i]);
-			i += 2;
+			i += 1;
 		}
 		vertexList.push(vertexList[0]); // Ensure they are looped
 		while(i < sector.length){
 			neighbors.push(sector[i]);
-			i += 2;
+			i += 1;
 		}
 		sectors.push(new Sector(floor, ceiling, vertexList, neighbors));
 	}
@@ -75,46 +78,84 @@ function loadData() {
 	player = new Player(data.player[0], data.player[1], 0, new Vector3D(0,0,0), data.player[2], 0, data.player[3]);
 }
 
-function vLine(x, y1, y2, topColor, middleColor, bottomColor) {
+function vLine(x, y1, y2, topColor, middleColor, bottomColor) {	
+	y1 = (y1 || 0).clamp(0, H-1);
+	y2 = (y2 || 0).clamp(0, H-1);
 	
+	if(y1 === y2) { 
+		//drawLine(x, y1, x, y1, middleColor);
+	} else if(y2 > y1){
+		drawLine(x, 0, x, y1, topColor);
+		drawLine(x, y1 + 2, x, y2 - 1, middleColor);
+		drawLine(x, y2, x, H-1, bottomColor);
+	}
+}
+
+function drawLine(x1, y1, x2, y2, color) {	
+	var line = document.createElementNS(xmlns, "line");
+	line.setAttributeNS(null,"x1",x1);
+	line.setAttributeNS(null,"y1",y1);
+	line.setAttributeNS(null,"x2",x2);
+	line.setAttributeNS(null,"y2",y2);
+	line.setAttributeNS(null,"style", "stroke: " + color);
+	mainrenderscene.appendChild(line);
+}
+
+function drawRect(x1, y1, x2, y2, color) {	
+	var rect = document.createElementNS(xmlns, "rect");
+	rect.setAttributeNS(null,"x",x1);
+	rect.setAttributeNS(null,"y",y1);
+	rect.setAttributeNS(null,"width",x2 - x1);
+	rect.setAttributeNS(null,"height",y2 - y1);
+	rect.setAttributeNS(null,"fill", color);
+	mainrenderscene.appendChild(rect);
 }
 
 function drawScreen() {
+	while (mainrenderscene.lastChild) {mainrenderscene.removeChild(mainrenderscene.lastChild); }
 	var sectorQueue = [];
-	sectorQueue.push(player.sector);
+	sectorQueue.push({ sectorno: player.sector, sx1: 0, sx2: W-1});
+	var ytop = [];
+	var ybottom = [];
+	for(var i = 0; i < W + 1; i += 1){ ytop.push(0); }
+	for(var i = 0; i < W + 1; i += 1){ ybottom.push(H - 1); }
 	renderedSectors = [];
 	
-	while (sectorQueue.length > 0) {		
-		var currentSector = sectorQueue.shift();
+	while (sectorQueue.length > 0) {
+		var currentRenderOp = sectorQueue.shift();
+		var currentSector = sectors[currentRenderOp.sectorno];
+		var sx1 = currentRenderOp.sx1;
+		var sx2 = currentRenderOp.sx2;
 		
 		for(var vertexIndex = 0; vertexIndex < currentSector.vertices.length - 1; vertexIndex+=1) {
-			var currentVertex = currentSector.vertices[vertexIndex];
-			var nextVertex = currentSector.vertices[vertexIndex + 1];
+			var currentVertex = vertices[currentSector.vertices[vertexIndex + 0]];
+			var nextVertex = vertices[currentSector.vertices[vertexIndex + 1]];
 			
 			// Get endpoints relative to player			
 			var vx1 = currentVertex.x - player.x, vy1 = currentVertex.y - player.y;		
-			var vx1 = nextVertex.x - player.x, vy1 = nextVertex.y - player.y;
-			var pcos = pcos = player.anglecos(), psin = player.anglesin();
-			
+			var vx2 = nextVertex.x - player.x, vy2 = nextVertex.y - player.y;
+			var pcos = player.anglecos(), psin = player.anglesin();			
+			var tx1 = vx1 * psin - vy1 * pcos,  tz1 = vx1 * pcos + vy1 * psin;
+			var tx2 = vx2 * psin - vy2 * pcos,  tz2 = vx2 * pcos + vy2 * psin;
 			// Check if wall is visible
 			if(tz1 <= 0 && tz2 <= 0) continue;
 			
 			// Partial clip
-			 if(tz1 <= 0 || tz2 <= 0)
+			if(tz1 <= 0 || tz2 <= 0)
 			{
 				var nearz = 1e-4, farz = 5, nearside = 1e-5, farside = 20.;
 				// Find an intersection between the wall and the approximate edges of player's view
-				i1 = intersect(tx1,tz1,tx2,tz2, -nearside,nearz, -farside,farz);
-				i2 = intersect(tx1,tz1,tx2,tz2,  nearside,nearz,  farside,farz);
+				i1 = intersect(new Vector2D(tx1,tz1), new Vector2D(tx2,tz2), new Vector2D(-nearside,nearz), new Vector2D(-farside,farz));
+				i2 = intersect(new Vector2D(tx1,tz1), new Vector2D(tx2,tz2), new Vector2D(nearside,nearz), new Vector2D(farside,farz));
 				if(tz1 < nearz) { if(i1.y > 0) { tx1 = i1.x; tz1 = i1.y; } else { tx1 = i2.x; tz1 = i2.y; } }
 				if(tz2 < nearz) { if(i1.y > 0) { tx2 = i1.x; tz2 = i1.y; } else { tx2 = i2.x; tz2 = i2.y; } }
 			}
 			
 			// Perspective transformation */
-			var xscale1 = hfov / tz1, yscale1 = vfov / tz1;    var x1 = W/2 - tx1 * xscale1;
-			var xscale2 = hfov / tz2, yscale2 = vfov / tz2;    var x2 = W/2 - tx2 * xscale2;
+			var xscale1 = hfov / tz1, yscale1 = vfov / tz1; var x1 = W/2 - tx1 * xscale1;
+			var xscale2 = hfov / tz2, yscale2 = vfov / tz2; var x2 = W/2 - tx2 * xscale2;
 			
-			if(x1 >= x2 || x2 < now.sx1 || x1 > now.sx2) continue; // Only render if it's visible
+			if(x1 >= x2 || x2 < sx1 || x1 > sx2) continue; // Only render if it's visible
 			/* Acquire the floor and ceiling heights, relative to where the player's view is */
 			var yceil  = currentSector.ceiling  - player.z;
 			var yfloor = currentSector.floor - player.z;
@@ -126,14 +167,39 @@ function drawScreen() {
 			var y1a  = H/2 - yaw(yceil, tz1) * yscale1,  y1b = H/2 - yaw(yfloor, tz1) * yscale1;
 			var y2a  = H/2 - yaw(yceil, tz2) * yscale2,  y2b = H/2 - yaw(yfloor, tz2) * yscale2;
 			
-			//TODO continue
+			var beginx = Math.max(x1, sx1), endx = Math.min(x2, sx2);
 			
+			if(currentSector.neighbors[vertexIndex] >= 0) {
+				drawRect(beginx, y1a, endx, y1b, "red");
+			 } else {
+				drawRect(beginx, y1a, endx, y1b, "white");
+			}
+			
+			drawRect(beginx, 0, endx, y1a.clamp(0,H), "grey");
+			drawRect(beginx, y1b.clamp(0,H), endx, H, "green");
+			
+		
+			// for(var x = beginx + 1; x <= endx; x+=1) {				
+				// var ya = (x - x1) * (y2a-y1a) / (x2-x1) + y1a, cya = ya.clamp(ytop[x],ybottom[x]); // top
+				// var yb = (x - x1) * (y2b-y1b) / (x2-x1) + y1b, cyb = yb.clamp(ytop[x],ybottom[x]); // bottom
+				
+				// Render ceiling: everything above this sector's ceiling height.
+				// vLine(x, ytop[x], cya-1, "grey" ,"lightgrey","grey");
+				// Render floor: everything below this sector's floor height.
+				// vLine(x, cyb+1, ybottom[x], "green","lightgreen","green");
+				// Render wall:
+				// if(currentSector.neighbors[vertexIndex] >= 0) {
+					// vLine(x, cya, cyb, "black", "red", "black");
+				// } else {
+					// vLine(x, cya, cyb, "black", "white", "black");
+				// }
+			// }
+				
+			if (currentSector.neighbors[vertexIndex] >= 0 && !renderedSectors.includes(currentSector.neighbors[vertexIndex]) && endx >= beginx){
+				// sectorQueue.push({ sectorno: currentSector.neighbors[vertexIndex], sx1: 0, sx2: W-1})
+			}
 		}
-		
-		
-		// for neighbours
-			// if renderedSectors includes(neighbor)
-			// push queue
+		renderedSectors.push(currentRenderOp.sectorno);		
 	}
 	
 }
@@ -186,10 +252,10 @@ class Player {
 	}
 	
 	anglesin() {
-		return Math.sin(angle);
+		return Math.sin(this.angle);
 	}
 	
 	anglecos() {
-		return Math.cos(angle);
+		return Math.cos(this.angle);
 	}
 }
